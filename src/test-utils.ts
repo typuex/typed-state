e529596c0a6506dbac1a0c3f336da09e5d786641
mehboodian <xeptore@gmail.com>
2021-01-26 23:32:44 +0330

    test: add test helper types

---
+++ src/test-utils.ts
@@ -0,0 +1,7 @@
+type Equals<A1, A2> = (<A>() => A extends A2 ? true : false) extends (<A>() => A extends A1 ? true : false)
+  ? true
+  : false;
+
+export declare function check<Type, Expect, Outcome extends boolean> (): Equals<Equals<Type, Expect>, Outcome>;
+
+export declare function checks (toBeChecked: true[]): void;
