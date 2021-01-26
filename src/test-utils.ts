e4f17efca0de83275d43a0f05e9d9944eab31f86
mehboodian <xeptore@gmail.com>
2021-01-26 23:45:57 +0330

    doc: add test-utils.ts documentation header comments

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Type helpers to make testing generated types possible.
+ * Used in `*.spec.ts` files.
+ *
+ * Credit: https://github.com/millsp/ts-toolbelt/blob/master/src/Test.ts
+ */
 type Equals<A1, A2> = (<A>() => A extends A2 ? true : false) extends (<A>() => A extends A1 ? true : false)
   ? true
   : false;
